2384669605098158433fe0458d5cceba5f608656
Tim Brust <ratchet.player@gmx.de>
2018-05-19 02:27:00 +0200

    fix: remove debug file

--- src/main/java/net/gravitydevelopment/updater/Test.java
+++
@@ -1,9 +0,0 @@
-package net.gravitydevelopment.updater;
-
-import net.gravitydevelopment.updater.Updater.UpdateType;
-
-public class Test {
-    public static void main(String args[]) {
-        new Updater(null, 35890, null, UpdateType.NO_DOWNLOAD, false);
-    }
-}
